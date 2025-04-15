08adb84f8d4043056e41f36de375248eaf2baae4
digi369 <digi24@usa.com>
2025-04-15 04:15:31 -0400

    visys.js

---
+++ visys.js
@@ -0,0 +1,5 @@
+var emailPart1 = "nw369";
+var emailPart2 = "proton.me";
+var emailPart3 = "cs";
+var emailPart4 = "visys[dot]ir";
+document.write("<a href='mailto:" + emailPart1 + "@" + emailPart2 + "'>" + emailPart3 + "[at]" + emailPart4 + "</a>");
