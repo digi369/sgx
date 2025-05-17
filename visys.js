2e7633552ed36f971203cbfc5e583561fbb51f61
digi369 <digi24@usa.com>
2025-05-17 05:15:40 -0400

    Update visys.js

--- visys.js
+++ visys.js
@@ -1,5 +1,5 @@
-var emailPart1 = "nw369";
-var emailPart2 = "proton.me";
+var emailPart1 = "info";
+var emailPart2 = "visys.ir";
 var emailPart3 = "cs";
 var emailPart4 = "visys[dot]ir";
 document.write("<a href='mailto:" + emailPart1 + "@" + emailPart2 + "'>" + emailPart3 + "[at]" + emailPart4 + "</a>");
