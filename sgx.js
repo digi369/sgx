0bd93b511ac31840109083c3e9d1f9d23d4bf8e8
digi369 <digi24@usa.com>
2025-05-17 05:13:19 -0400

    Create sgx.js

---
+++ sgx.js
@@ -0,0 +1,5 @@
+var emailPart1 = "info";
+var emailPart2 = "sterngold.ir";
+var emailPart3 = "info";
+var emailPart4 = "sterngold[dot]ir";
+document.write("<a href='mailto:" + emailPart1 + "@" + emailPart2 + "'>" + emailPart3 + "[at]" + emailPart4 + "</a>");
